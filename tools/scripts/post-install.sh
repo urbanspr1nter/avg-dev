8146f2db8fb626e8a83ea926ca1808d1f5db5455
Roger <urbanspr1nter@gmail.com>
2024-07-31 16:53:12 -0700

    Added statement to source bashrc at the end of the post installation script

--- tools/scripts/post-install.sh
+++ tools/scripts/post-install.sh
@@ -10,3 +10,5 @@ curl https://get.volta.sh | bash
 
 # Install node using volta from the absolute path
 $HOME/.volta/bin/volta install node@lts
+
+echo "All done! You should run this command to finish up: source ~/.bashrc"
