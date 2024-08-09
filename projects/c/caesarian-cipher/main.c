fa7a69e86653ece010155b7e51746e9857dd7039
Roger <urbanspr1nter@gmail.com>
2024-08-08 17:07:57 -0700

    Handling edge cases

--- projects/c/caesarian-cipher/main.c
+++ projects/c/caesarian-cipher/main.c
@@ -16,6 +16,16 @@ int main() {
     char input;
     scanf("%c", &input);
 
+    if (input < 'a' || input > 'z') {
+        printf("Please input a character between a and z (lower case)\n");
+        return 1;
+    }
+
+    if (input + shift > 'z') {
+        printf("Shift is too large to handle encoding of this character. Shift is currently %d.\n", shift);
+        return 1;
+    }
+
     char result = input + shift;
 
     printf("%c\n", result);
