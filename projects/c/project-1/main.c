336c77039fdbc626e9c9368b8f7f8c3330748dad
Roger <urbanspr1nter@gmail.com>
2024-08-24 10:35:38 -0700

    Webby!

--- projects/c/project-1/main.c
+++ projects/c/project-1/main.c
@@ -2,18 +2,69 @@
 // Title: Hello, world!
 // Content: Welcome to my new personal web page. :)
 
+/**
+ * gcc main.c -o webby
+ * 
+ * ./webby -t "My Portfolio" -h "Roger's Coding Portfolio" -c contents.txt -o /path/to/index.html
+ */
+
+
 #include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char* argv[]) {
-  puts("<html>");
-  puts("<head>");
-  puts("<title>My website</title>");
-  puts("</head>");
-  puts("<body>");
-  puts("<h1>Hello, world</h1>");
-  puts("<p>Welcome to my new personal web page. :)</p>");
-  puts("</body>");
-  puts("</html>");
+  char* page_title;
+  char* page_heading;
+  char* page_contents_filename;
+  char* page_output_filename;
+
+  char ch;
+  while ((ch = getopt(argc, argv, "t:h:c:o:")) != EOF) {
+    switch (ch) {
+      case 't':
+        page_title = optarg;
+        break;
+      case 'h':
+        page_heading = optarg;
+        break;
+      case 'c':
+        page_contents_filename = optarg;
+        break;
+      case 'o':
+        page_output_filename = optarg;
+        break;
+      default:
+        break;
+    }
+  }
+
+  // Create an output file
+  FILE *output_file = fopen(page_output_filename, "w");
+
+  fprintf(output_file, "<html>");
+  fprintf(output_file, "<head>");
+  fprintf(output_file, "<title>%s</title>\n", page_title);
+  fprintf(output_file, "</head>");
+  fprintf(output_file,"<body>");
+  fprintf(output_file, "<h1>%s</h1>\n", page_heading);
+  
+  // declare a line buffer of 1000 characters
+  char line_buffer[1000];
+
+  // open the file found at page_contents_filename
+  FILE *contents_file = fopen(page_contents_filename, "r");
+
+  // simply for now, print out the contents
+  while (fscanf(contents_file, "%999[^\n]\n", line_buffer) == 1) {
+    fprintf(output_file, "<p>%s</p>\n", line_buffer);
+  }
+
+  fclose(contents_file);
+
+  fprintf(output_file, "</body>");
+  fprintf(output_file, "</html>");
+
+  fclose(output_file);
 
   return 0;
 }
