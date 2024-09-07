6dec70aad3b745905473a10836e08ebf223606ce
Roger <urbanspr1nter@gmail.com>
2024-09-07 11:52:18 -0700

    More LinkedList and malloc

--- projects/c/linked-list/main.c
+++ projects/c/linked-list/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct island {
   char* name;
@@ -10,54 +12,50 @@ typedef struct island {
 void display(island* start) {
   island* i = start;
   for (; i != NULL; i = i->next) {
-    printf("Name: %s open: %s-%s\n", i->name, i->opens, i->closes);
+    printf("Name: %s\n Open: %s-%s\n", i->name, i->opens, i->closes);
+  }
+}
+
+island* create(char* name) {
+  island* i = malloc(sizeof(island));
+
+  i->name = strdup(name);
+  i->opens = "09:00";
+  i->closes = "17:00";
+  i->next = NULL;
+
+  return i;
+}
+
+void release(island* start) {
+  island* i = start;
+  island* next = NULL;
+
+  for (; i != NULL; i = next) {
+    next = i->next;
+    free(i->name);
+    free(i);
   }
 }
 
 int main(int argc, char* argv[]) {
-  island amity = {
-    .name = "Amity",
-    .opens = "09:00",
-    .closes = "17:00",
-    .next = NULL
-  };
+  island* start = NULL;
+  island* i = NULL;
+  island* next = NULL;
 
-  island craggy = {
-    .name = "Craggy",
-    .opens = "09:00",
-    .closes = "17:00",
-    .next = NULL
-  };
+  char name[80];
+  for (; fgets(name, 80, stdin) != NULL; i = next) {
+    next = create(name);
+    if (start == NULL) {
+      start = next;
+    }
+    if (i != NULL) {
+      i->next = next;
+    }
+  }
 
-  island isla_nublar = {
-    .name = "Isla Nublar",
-    .opens = "09:00",
-    .closes = "17:00",
-    .next = NULL
-  };
-
-  island shutter = {
-    .name = "Shutter",
-    .opens = "09:00",
-    .closes = "17:00",
-    .next = NULL
-  };
-
-  amity.next = &craggy;
-  craggy.next = &isla_nublar;
-  isla_nublar.next = &shutter;
-
-  island skull = {
-    .name = "Skull",
-    .opens = "09:00",
-    .closes = "17:00",
-    .next = NULL
-  };
-
-  isla_nublar.next = &skull;
-  skull.next = &shutter;
-
-  display(&amity);
+  display(start);
+  release(start);
 
   return 0;
 }
