fe2112b029871c24f462a98858db9195aace62de
Roger <>
2024-12-05 10:37:00 -0800

    Search and shuffle

--- projects/python/study-buddy/card_stack.py
+++ projects/python/study-buddy/card_stack.py
@@ -1,3 +1,5 @@
+import random
+
 class CardStack:
 
     def __init__(self):
@@ -5,6 +7,29 @@ class CardStack:
 
     def add(self, card):
         self._collection.append(card)
+
+    def draw(self):
+        if len(self._collection) == 0:
+            raise ValueError("You can't draw from an empty deck.")
+
+        result = self._collection.pop(0)
+
+        return result
+
+    def shuffle(self):
+        random.shuffle(self._collection)
+
+    def search(self, query):
+        """
+        Searches the deck for cards that matches the query
+        """
+        result_deck = CardStack()
+
+        for card in self._collection:
+            if query.lower() in card.get_description().lower():
+                result_deck.add(card)
+
+        return result_deck
 
     def get_collection(self):
         return list(self._collection)
--- projects/python/study-buddy/main.py
+++ projects/python/study-buddy/main.py
@@ -8,11 +8,16 @@ with open("data.json") as fp:
 
     data_as_obj = json.loads(data_contents)
 
+    archive_stack = CardStack()
     card_stack = CardStack()
     for content in data_as_obj:
         new_card = Card(content["label"], content["description"], content["category"])
         card_stack.add(new_card)
 
-    card_collection = card_stack.get_collection()
-    for card in card_collection:
+    card_stack.shuffle()
+
+    searched_stack = card_stack.search("APPLICATION")
+
+    for card in searched_stack.get_collection():
         print(card.to_string())
+
