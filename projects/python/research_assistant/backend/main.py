8ee77c94bc48b3d7e0bfa16b982da4b67433c611
Roger <urbanspr1nter@gmail.com>
2025-06-29 10:00:41 -0700

    Chat client

--- projects/python/research_assistant/backend/main.py
+++ projects/python/research_assistant/backend/main.py
@@ -1,8 +1,15 @@
-from flask import Flask, jsonify
+import json
+from flask import Flask, jsonify, request, Response, stream_with_context
 from flask_cors import CORS
+from research_assistant.llm.client import BasicChatClient
 
 app = Flask(__name__)
 CORS(app)
+
+chat_client = BasicChatClient(
+    base_url="http://192.168.1.77:8000/v1",
+    model="qwen/qwen2.5-coder-14b"
+)
 
 @app.route("/hello", methods=["GET"])
 def hello():
@@ -10,6 +17,28 @@ def hello():
         "status": "ok",
         "message": "hello, world"
     }), 200
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    data = request.get_json()
+    query = data.get("query", "")
+
+    if not query:
+        return jsonify({
+            "status": "required_field_empty", 
+            "message": "missing query field"
+        }), 400
+
+    def generate():
+        for chunk in chat_client.send_message(query):
+            yield json.dumps({"chunk": chunk}) + "\n"
+
+        yield json.dumps({"chunk": "\n\n"}) + "\n"
+
+    return Response(
+        stream_with_context(generate()),
+        mimetype="application/json"
+    ), 200
 
 if __name__ == "__main__":
     app.run(
---
+++ projects/python/research_assistant/backend/research_assistant/llm/client.py
@@ -0,0 +1,63 @@
+import openai
+from openai import Stream
+from openai.types.chat import ChatCompletionChunk
+
+"""
+A basic LLM client that interfaces with an OpenAI compatible endpoint.
+
+Initialization Arguments:
+    base_url - the base API url ending with /v1
+    api_key - the api key, defaults to 'none'
+    model - the model to chat with
+
+Methods:
+    send_message(str, opts={})
+"""
+
+class BasicChatClient:
+    def __init__(self, base_url: str, model: str, api_key="none"):
+        self.base_url = base_url
+        self.model = model
+
+        self.client = openai.Client(
+            base_url=self.base_url,
+            api_key=api_key
+        ) 
+
+        self.messages = []
+        self.system_prompt = "You are a helpful research assistant."
+
+        self.messages.append({
+            "role": "system",
+            "content": self.system_prompt
+        })
+
+    def send_message(self, message: str, opts={}):
+        self.messages.append({
+            "role": "user",
+            "content": message
+        })
+
+        stream: Stream = self.client.chat.completions.create(
+            model=self.model,
+            messages=self.messages,
+            stream=True,
+            temperature=opts.get("temperature", 1.0)
+        )
+ 
+        assistant_message = ""
+        for chunk in stream:
+            delta_content = chunk.choices[0].delta.content
+            if delta_content is None:
+                break
+
+            assistant_message += delta_content
+
+            yield delta_content 
+
+        self.messages.append({
+            "role": "assistant",
+            "content": assistant_message
+        })
+
+
